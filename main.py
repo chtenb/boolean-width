876a3e5c3b4335e3273d21ef4e7d08b451a518b4
Chiel92 <ctje92@gmail.com>
2014-09-29 14:32:31 +0200

    created simple testsuites

--- main.py
+++ main.py
@@ -3,36 +3,38 @@ from random import randint
 
 from plot import plot_bipartite_graph, plot_graph
 from bipartite import Bipartite, Graph
-from mis import bron_kerbosch_mc
+from mis import bron_kerbosch_mis, bron_kerbosch_mc
 
 
-#graph = Graph.generate_random(10, 10)
-#complement = graph.complement()
+def mc_vs_mis():
+    for _ in range(20):
+        nr_vertices = 20
+        nr_edges = randint(0, int(nr_vertices * (nr_vertices - 1) / 2))
+        graph = Graph.generate_random(nr_vertices, nr_edges)
 
-# print(graph)
-# print(len(list(bron_kerbosch(graph))))
-# print(len(list(bron_kerbosch(complement))))
+        mis = list(bron_kerbosch_mis(graph))
+        nr_mis = len(mis)
+        mc = list(bron_kerbosch_mc(graph))
+        nr_mc = len(mc)
+        print('{} - {} = {}'.format(nr_mis, nr_mc, abs(nr_mis - nr_mc)))
 
-for _ in range(10):
-    nr_vertices = 15
-    nr_edges = randint(10, 50)
-    graph = Bipartite.generate_random(nr_vertices, nr_edges)
-    bipartite_complement = graph.bipartite_complement()
 
-    # print(graph)
-    booldim1 = len(list(bron_kerbosch_mc(graph)))
-    booldim2 = len(list(bron_kerbosch_mc(bipartite_complement)))
-    #print('{}, {}'.format(booldim1, booldim2))
-    print(abs(booldim1 - booldim2))
-    #if abs(booldim1 - booldim2) > 1:
-        #print('!!!!')
+def mc_vs_mis_bipartite():
+    for _ in range(20):
+        nr_vertices = 20
+        nr_edges = randint(0, int((nr_vertices / 2) ** 2))
+        graph = Bipartite.generate_random(nr_vertices, nr_edges)
+
+        mis = list(bron_kerbosch_mis(graph))
+        nr_mis = len(mis)
+        mc = list(bron_kerbosch_mc(graph))
+        nr_mc = len(mc)
+        print('{} - {} = {}'.format(nr_mis, nr_mc, abs(nr_mis - nr_mc)))
+
+mc_vs_mis()
 
 #size = (512, 512)
 #im = Image.new('RGB', size, 'white')
-
 #plot_graph(im, graph, color=(178, 0, 0))
 #plot_graph(im, complement, color=(0, 0, 178))
-#plot_bipartite_graph(im, graph)
-#plot_bipartite_graph(im, bipartite_complement, color=(0, 0, 128))
-
 #im.save('output/test.png', 'png')
