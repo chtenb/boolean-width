9425900739f6a44490a3fc45656197ee3189cc34
Chiel92 <ctje92@gmail.com>
2014-10-02 11:45:15 +0200

    generating biconvex bipartite graphs is not trivial

---
+++ biconvexbipartite.py
@@ -0,0 +1,25 @@
+
+from random import randint
+from numpy import diff
+
+from bipartite import Bipartite
+from graph import Vertex
+
+
+class BiconvexBipartite(Bipartite):
+
+    def verify_biconvexity(self):
+        """Check that we are indeed biconvex."""
+        def verify_group(group):
+            for v in group:
+                ids = [w.identifier for w in v.neighbours]
+                ids.sort()
+                differences = diff(ids)
+                if differences.size > 0 and max(differences) > 1:
+                    return False
+
+        return verify_group(self.group1) and verify_group(self.group2)
+
+    @staticmethod
+    def generate_random(nr_vertices, nr_edges):
+        return NotImplemented
