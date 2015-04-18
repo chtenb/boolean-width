e38011b12f4de0f43b5552c3abef36769601bd77
Chiel92 <ctje92@gmail.com>
2015-04-18 21:10:31 +0200

    Data can be plotted

--- booleanwidth/experiments/lboolw_exact_vs_heuristic.pyx
+++ booleanwidth/experiments/lboolw_exact_vs_heuristic.pyx
@@ -2,22 +2,24 @@ from ..graph import Graph
 from ..graph128 import to128
 from ..lboolw import compute_lboolw
 
-from numpy import arange
+from numpy import arange, mean
+import matplotlib.pyplot as plt
+
 import shutil
 import os
 import glob
+import pickle
 
 
 def run():
     graphsize = 10
     p_values = [round(p, 5) for p in arange(0.05, 1, 0.05)]
-    samples = 2
+    samples = 20
     directory = 'output/lboolw-exact-vs-heuristic/'
 
-    generate_random_graphs(graphsize, p_values, samples, directory)
-    data = compute_data(directory)
-
-    print(data)
+    #generate_random_graphs(graphsize, p_values, samples, directory)
+    #compute_data(directory)
+    plot_data(directory)
 
 
 def generate_random_graphs(graphsize, p_values, samples, outputdir):
@@ -51,9 +53,25 @@ def compute_data(input_dir):
 
         data[p].append(lboolw)
 
-    return data
+    avg_data = []
+    for p, lboolw_values in data.items():
+        avg_data.append((p, mean(lboolw_values)))
+
+    pickle.dump(avg_data, open(input_dir + 'data', 'wb'))
 
 
-def plot_data():
-    ...
+def plot_data(input_dir):
+    data = pickle.load(open(input_dir + 'data', 'rb'))
+
+    plt.subplot(121)
+    #styles = ['ro', 'g^', 'bs', 'cD', 'mh', 'k+']
+    plt.plot([p[0] for p in data], [p[1] for p in data], 'ro', label='lboolw')
+
+    plt.axis([-.05, 1, -1, 50])
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., numpoints=1)
+    plt.title('Title')
+    plt.xlabel('Edge probability')
+    plt.ylabel('Average linear boolean width')
+    plt.grid(True)
+    plt.show()
 
